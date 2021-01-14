7601cf821d08957cc621559bff13c83cb1b2efee
theo <teddyhoover@gmail.com>
2021-01-14 15:46:59 -0600

    commit tree 8faa53cd239285259c3fa82f3e3f286dbb989255

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,107 @@
+require 'pry'
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      SQL
+
+    DB[:conn].execute(sql).map do |row|
+      new_from_db(row)
+    end
+    #binding.pry
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = '9'
+      SQL
+
+    DB[:conn].execute(sql)
+    #binding.pry
+  end
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade != '12'
+      SQL
+
+    DB[:conn].execute(sql).map do |row|
+      new_from_db(row)
+    end
+    #binding.pry
+  end
+
+  def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade = 10
+        ORDER BY id
+        LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, number).map do |row|
+      new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade = 10
+        ORDER BY id
+        LIMIT 1
+    SQL
+
+    row = DB[:conn].execute(sql)[0]
+    new_from_db(row)
+    
+  end
+
+  def self.all_students_in_grade_X(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      ORDER BY id 
+      SQL
+
+    DB[:conn].execute(sql, number).map do |row|
+      new_from_db(row)
+    end
+
+  end
+
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      SQL
+
+    row = DB[:conn].execute(sql, name)[0]
+    new_from_db(row)
   end
   
   def save
